5e302645efc49c1c2a3d1625df5b2ed4b6e7c5d5
phat 1166 <trangphat16.cantho@gmail.com>
2023-05-05 13:28:57 +0700

    Xoa nut tim kiem

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -20,19 +20,10 @@ export default {
         <input
             type="text"
             class="form-control"
-            placeholder="Nhập thông tin cần tìm"
+            placeholder="Nhập thông tin sách muốn tìm"
             :value="modelValue"
             @input="updateModelValue"
             @keyup.enter="submit"
         />
-        <div class="input-group-append">
-            <button
-                class="btn btn-outline-secondary"
-                type="button"
-                @click="submit"
-            >
-                <i class="fas fa-search"></i>Tìm kiếm
-            </button>
-        </div>
     </div>
 </template>
